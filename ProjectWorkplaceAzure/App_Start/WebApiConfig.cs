9f050e9c656d7d45554d147bec0b78013305717b
Eros Niko Cas Alvarez <iamerosniko@yahoo.com>
2017-09-06 14:09:39 +0800

    up

--- ProjectWorkplaceAzure/App_Start/WebApiConfig.cs
+++ ProjectWorkplaceAzure/App_Start/WebApiConfig.cs
@@ -10,21 +10,23 @@ namespace ProjectWorkplaceAzure
     {
         public static void Register(HttpConfiguration config)
         {
-            //enable cors
-            var cors = new EnableCorsAttribute("*", "*", "*");
-            config.EnableCors(cors);
             // Web API configuration and services
             var json = config.Formatters.JsonFormatter;
             json.SerializerSettings.PreserveReferencesHandling = Newtonsoft.Json.PreserveReferencesHandling.Objects;
             config.Formatters.Remove(config.Formatters.XmlFormatter);
             // Web API routes
             config.MapHttpAttributeRoutes();
-
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            //enable cors
+            EnableCorsAttribute cors = new EnableCorsAttribute("*", "*", "*");
+            config.EnableCors(cors);
+            //for chrome to enable preflight request 
+            //--disable-web-security --user-data-dir
         }
     }
 }
--- ProjectWorkplaceAzure/Migrations/Configuration.cs
+++ ProjectWorkplaceAzure/Migrations/Configuration.cs
@@ -1,12 +1,9 @@
 namespace ProjectWorkplaceAzure.Migrations
 {
-    using ProjectWorkplaceAzure.Models;
-using System;
-using System.Data.Entity;
-using System.Data.Entity.Migrations;
-using System.IO;
-using System.Linq;
-using System.Reflection;
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
 
     internal sealed class Configuration : DbMigrationsConfiguration<ProjectWorkplaceAzure.Models.ProjectWorkplaceAzureContext>
     {
@@ -29,37 +26,6 @@ using System.Reflection;
             //      new Person { FullName = "Rowan Miller" }
             //    );
             //
-            string codeBase = Assembly.GetExecutingAssembly().CodeBase;
-            UriBuilder uri = new UriBuilder(codeBase);
-            string path = Uri.UnescapeDataString(uri.Path);
-            var baseDir = Path.GetDirectoryName(path) + "\\Migrations\\PW_ViewEmployeeScore.View.sql";
-
-            context.Database.ExecuteSqlCommand(File.ReadAllText(baseDir));
-
-            baseDir = Path.GetDirectoryName(path) + "\\Migrations\\PW_VW_QUESTIONS.View.sql";
-            context.Database.ExecuteSqlCommand(File.ReadAllText(baseDir));
-
-            //context.PW_Teams.AddOrUpdate(x => x.TeamID,
-            //       new PW_Teams()
-            //       {
-            //            TeamID = new Guid ("52865e93-bba1-4e8b-a4dc-c442386d7b5c"),
-            //            TeamDesc = "DIS - Enterprise Solutions Development",
-            //            IsActive = true
-            //       }
-
-            //);
-
-            //context.PW_Leaders.AddOrUpdate(x => x.LeaderID,
-            //       new PW_Leaders()
-            //       {
-            //           LeaderID = 17,
-            //           LeaderName = "Gian Karlo Villaluz",
-            //           ManagerID = null,
-            //           LeaderResourceID = new Guid("8a43576a-264c-40ad-a9f8-a45390efa421"),
-            //           ManagerResourceID = new Guid("594370e4-4f40-4695-b337-348e66fd349a")
-            //       }
-
-            //);
         }
     }
 }
